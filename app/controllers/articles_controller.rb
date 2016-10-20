1ec5bd006001341dc8b94d03d25fa6c0bb9b2fdd
pablok47 <pkaparis@gmail.com>
2016-10-20 20:23:02 +0000

    modified the articles controller by adding the edit action with the update...created edit page as a result

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+    
+  end
+  
   def create
 
     @article = Article.new(article_params)
@@ -16,9 +21,21 @@ class ArticlesController < ApplicationController
  
   end
   
+  def update 
+    
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+    
+  end
+  
   def show
   
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:id]) 
   
   end
   
